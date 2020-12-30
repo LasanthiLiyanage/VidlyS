682b5f03ace5fc0a9bc6c9ee43b87af786e1936f
LasanthiLiyanage <75748796+LasanthiLiyanage@users.noreply.github.com>
2020-12-31 01:11:12 +0530

    Add project files.

---
+++ VidlyS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyS.Startup))]
+namespace VidlyS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
